f47441ed04af71212e18ed3047c9b7167ab5a5e5
Marina Friauf <marina506506506@gmail.com>
2023-04-21 11:31:25 +0200

    Initial commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,19 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
-  return arr;
-}
+  let d = Math.pow(b,2) - 4 * a * c;
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  if (d == 0) {
+    let solution = (-b/(2*a));
+    arr.push(solution);
+  } 
+
+  if (d > 0) {
+    let solution = (-b + Math.sqrt(d) )/(2*a);
+    arr.push(solution);
+    solution = (-b - Math.sqrt(d) )/(2*a);
+    arr.push(solution);
+  }
+
+  return arr;
 }
