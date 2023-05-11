ac4d6dcadec0d0a3edce6a06d5368ee97e3ed845
Marina Friauf <marina506506506@gmail.com>
2023-05-12 00:09:37 +0200

    Задача 1

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,32 @@
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function(...marks) {
+	this.marks !== undefined ? this.marks.push(...marks) : console.log("отчислен");
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.getAverage = function() {
+	if (this.marks !== undefined) {
+		return this.marks.reduce((acc, element, index, arr) => {
+			acc += element;
+			if (index === arr.length - 1) {
+				return acc / arr.length;
+			}
+			return acc;
+		}, 0);
+	} else return 0;
 }
 
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+	this.excluded = reason;
+	delete this.subject;
+	delete this.marks;
 }
